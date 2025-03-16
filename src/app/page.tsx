918b5298957533f041ffa07a23452d5d8caf0554
unsortedkgpian <theadityakumar2810@gmail.com>
2025-03-16 21:25:05 +0530

    error resolve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import BusinessLoanSection from "@/components/BusinessLoanSection";
 import FeaturesSection from "@/components/FeaturesSection";
-// import Footer from "@/components/Footer";
+
 import FooterSection from "@/components/FooterSection";
 import Header from "@/components/Header";
 import HowItWorks from "@/components/HowItWorks";
@@ -10,8 +10,7 @@ import PartnerSection from "@/components/PartnerSection";
 import PersonalizedSection from "@/components/PersonalizedSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
 import TopBanner from "@/components/TopBanner";
-// import { Tenor_Sans } from "next/font/google";
-// import Image from "next/image";
+
 
 export default function Home() {
     return (
