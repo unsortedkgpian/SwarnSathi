c9943e4cf2e92b3b09714c1a7c5e651368a7fba5
unsortedkgpian <theadityakumar2810@gmail.com>
2025-04-13 22:05:18 +0530

    Faster response

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -1,22 +1,79 @@
-'use client';
+// 'use client';
 
+// import CareerBanner from "@/components/careers/CareerBanner";
+// import CareerSingleSection from "@/components/careers/CarrerSingleSection";
+// import React from "react";
+// import '../globals.css'
+// import JobApplicationForm from "@/components/careers/Application";
+// import { useSearchParams } from 'next/navigation';
+
+// const CareersPage = () => {
+//     const searchParams = useSearchParams();
+//     const position = searchParams.get('position');
+
+//     return (
+//         <>
+//             <CareerBanner />
+//             {/* <CareerSingleSection /> */}
+//             <JobApplicationForm defaultPosition={position || ''} />
+//         </>
+//     );"use client";
+
+//     import React, { Suspense } from "react";
+//     import CareerBanner from "@/components/careers/CareerBanner";
+//     import JobApplicationForm from "@/components/careers/Application";
+//     import { useSearchParams } from "next/navigation";
+
+//     const InnerCareersPage = () => {
+//         const searchParams = useSearchParams();
+//         const position = searchParams.get("position");
+
+//         return (
+//             <>
+//                 <CareerBanner />
+//                 <JobApplicationForm defaultPosition={position || ""} />
+//             </>
+//         );
+//     };
+
+//     const CareersPage = () => {
+//         return (
+//             <Suspense fallback={<div>Loading...</div>}>
+//                 <InnerCareersPage />
+//             </Suspense>
+//         );
+//     };
+
+//     export default CareersPage;
+
+// };
+
+// export default CareersPage;
+
+"use client";
+
+import React, { Suspense } from "react";
 import CareerBanner from "@/components/careers/CareerBanner";
-import CareerSingleSection from "@/components/careers/CarrerSingleSection";
-import React from "react";
-import '../globals.css'
 import JobApplicationForm from "@/components/careers/Application";
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams } from "next/navigation";
 
-const CareersPage = () => {
+const InnerCareersPage = () => {
     const searchParams = useSearchParams();
-    const position = searchParams.get('position');
+    const position = searchParams.get("position");
 
     return (
         <>
             <CareerBanner />
-            {/* <CareerSingleSection /> */}
-            <JobApplicationForm defaultPosition={position || ''} />
+            <JobApplicationForm defaultPosition={position || ""} />
         </>
+    );
+};
+
+const CareersPage = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <InnerCareersPage />
+        </Suspense>
     );
 };
 
